cf0ae47821e8baf2e2ae999c4164b7099e15fb05
PepGuasch <147140695+PepGuasch@users.noreply.github.com>
2023-12-28 17:42:59 +0100

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         // Declara un array para almacenar los 6 números
+        int[] numeros = new int[6];
+
+        // Solicita al usuario que ingrese 6 números
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("Ingrese el número {0}: ", i + 1);
+            // Lee el número ingresado por el usuario y lo convierte a entero
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Imprime los números en orden inverso
+        Console.WriteLine("\nNúmeros en orden inverso:");
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+
+        // Espera que el usuario presione una tecla antes de cerrar la aplicación
+        Console.ReadLine();
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Declarar un array para almacenar los nombres
+        string[] nombres = new string[100];
+        int contador = 0;
+
+        Console.WriteLine("Introduce nombres (escribe 'stop' para finalizar):");
+
+        // Bucle para pedir nombres hasta que se ingrese 'stop' o se alcance el límite de 100 nombres
+        while (contador < 100)
+        {
+            Console.Write("Nombre {0}: ", contador + 1);
+            string nombre = Console.ReadLine();
+
+            // Verificar si se debe detener la entrada de nombres
+            if (nombre.ToLower() == "stop")
+            {
+                break;
+            }
+
+            // Almacenar el nombre en el array
+            nombres[contador] = nombre;
+            contador++;
+        }
+
+        // Mostrar la lista de nombres
+        Console.WriteLine("\nLista de nombres introducidos:");
+
+        for (int i = 0; i < contador; i++)
+        {
+            Console.WriteLine(nombres[i]);
+        }
+
+        // Esperar que el usuario presione una tecla antes de cerrar la aplicación
+        Console.ReadLine();
+    }
+}
